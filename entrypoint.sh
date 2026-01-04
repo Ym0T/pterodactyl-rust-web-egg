478b2e768e713170279c4aeb30b7649db985b699
Tom <104158130+Ym0T@users.noreply.github.com>
2026-01-04 20:29:34 +0100

    Fix writable home for registry/cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ cd /home/container
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP
 
+# Cargo needs a writable home for registry/cache (binaries stay in /opt/rust)
+export CARGO_HOME="/home/container/.cargo"
+mkdir -p "$CARGO_HOME"
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo -e ":/home/container$ ${MODIFIED_STARTUP}"
